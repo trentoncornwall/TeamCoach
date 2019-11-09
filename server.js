8609af5152ee2db03d026974f12eed50a03f04f9
amazingefren <efrentheroker@hotmail.com>
2019-11-09 00:57:13 -0700

    package updates, manifest removed to console err cleanup

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/teamcoach", {
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {
