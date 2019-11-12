6643c613a2a7760bd2081355a9895825e67069ce
amazingefren <efrentheroker@hotmail.com>
2019-11-11 22:39:46 -0700

    minor compile warning in Admin.js addressed

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -13,7 +13,7 @@ class Admin extends Component {
     API.getUsers()
       .then(data => {
         var dataArr = [];
-        data.data.map(user => {
+        data.data.forEach(user => {
           dataArr.push(user);
         });
         this.setState({ userData: dataArr });
@@ -29,12 +29,6 @@ class Admin extends Component {
     return (
       <MainPanel>
         <h1>Admin</h1>
-        {/* 
-          "Expected to return a value in arrow function for function below, we can either
-          1. keep this and add a filter to ignore compile warnings,
-          2. change to a forEach function,
-          3. Ignore The warning"
-        */}
         {this.state.userData.map(user => (
           <UserComp
             id={user._id}
@@ -45,6 +39,7 @@ class Admin extends Component {
             username={user.username}
           />
         ))}
+
         <UserCreate />
       </MainPanel>
     );
