6d538a9773f2a42992e3ed0aaee6708117b347b4
amazingefren <efrentheroker@hotmail.com>
2019-11-12 02:45:51 -0700

    updated user creation and admin page to handle states globally for future func

--- client/src/components/Admin/User/Create/index.js
+++ client/src/components/Admin/User/Create/index.js
@@ -1,95 +1,55 @@
-import React, { Component } from "react";
-// import Axios from "axios";
-import API from "../../../../utils/API";
+import React from "react";
 
-class UserCreate extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    userType: 0,
-    username: "",
-    userPassword: ""
-  };
-
-  handleInputChange = event => {
-    const value = event.target.value;
-    const name = event.target.name;
-
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    // console.log(this.state);
-    if (
-      this.state.firstName &&
-      this.state.lastName &&
-      this.state.username &&
-      this.state.userPassword &&
-      this.state.userType
-    ) {
-      API.postUser({
-        fName: this.state.firstName,
-        lName: this.state.lastName,
-        username: this.state.username,
-        password: this.state.userPassword,
-        userType: this.state.userType
-      });
-      console.log({
-        fName: this.state.firstName,
-        lName: this.state.lastName,
-        username: this.state.username,
-        password: this.state.userPassword,
-        userType: this.state.userType
-      });
-    }
-  };
-
-  render() {
-    return (
-      <div>
-        <form>
-          First Name: <br />
-          <input
-            type="text"
-            name="firstName"
-            onChange={this.handleInputChange}
-          />
-          <br />
-          Last Name: <br />
-          <input
-            type="text"
-            name="lastName"
-            onChange={this.handleInputChange}
-          />
-          <br />
-          <select name="userType" onChange={this.handleInputChange}>
-            <option value="0">Base (0)</option>
-            <option value="1">Supervisor (1)</option>
-            <option value="2">Manager (2)</option>
-            <option value="3">Admin (3)</option>
-          </select>{" "}
-          <br />
-          Username: <br />
-          <input
-            type="text"
-            name="username"
-            onChange={this.handleInputChange}
-          />{" "}
-          <br />
-          Password: <br />
-          <input
-            type="password"
-            name="userPassword"
-            onChange={this.handleInputChange}
-          />
-          <input type="submit" value="Submit" onClick={this.handleSubmit} />
-        </form>
-      </div>
-    );
-  }
+function UserCreate(props) {
+  return (
+    <div>
+      <form>
+        First Name: <br />
+        <input
+          type="text"
+          name="firstName"
+          onChange={props.HIC}
+          value={props.state.firstName}
+        />
+        <br />
+        Last Name: <br />
+        <input
+          type="text"
+          name="lastName"
+          onChange={props.HIC}
+          value={props.state.lastName}
+        />
+        <br />
+        <select
+          name="userType"
+          onChange={props.HIC}
+          value={props.state.userType}
+        >
+          <option value="0">Base (0)</option>
+          <option value="1">Supervisor (1)</option>
+          <option value="2">Manager (2)</option>
+          <option value="3">Admin (3)</option>
+        </select>{" "}
+        <br />
+        Username: <br />
+        <input
+          type="text"
+          name="username"
+          onChange={props.HIC}
+          value={props.state.username}
+        />
+        <br />
+        Password: <br />
+        <input
+          type="password"
+          name="userPassword"
+          onChange={props.HIC}
+          value={props.state.userPassword}
+        />
+        <input type="submit" value="Submit" onClick={props.HS} />
+      </form>
+    </div>
+  );
 }
 
 export default UserCreate;
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -6,7 +6,48 @@ import UserCreate from "../components/Admin/User/Create";
 
 class Admin extends Component {
   state = {
+    firstName: "",
+    lastName: "",
+    userType: 0,
+    username: "",
+    userPassword: "",
     userData: []
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // console.log(this.state);
+    if (
+      this.state.firstName &&
+      this.state.lastName &&
+      this.state.username &&
+      this.state.userPassword &&
+      this.state.userType
+    ) {
+      API.postUser({
+        fName: this.state.firstName,
+        lName: this.state.lastName,
+        username: this.state.username,
+        password: this.state.userPassword,
+        userType: this.state.userType
+      });
+      console.log({
+        fName: this.state.firstName,
+        lName: this.state.lastName,
+        username: this.state.username,
+        password: this.state.userPassword,
+        userType: this.state.userType
+      });
+    }
   };
 
   loadUsers = () => {
@@ -40,7 +81,11 @@ class Admin extends Component {
           />
         ))}
 
-        <UserCreate />
+        <UserCreate
+          HIC={this.handleInputChange}
+          HS={this.handleSubmit}
+          state={this.state}
+        />
       </MainPanel>
     );
   }
