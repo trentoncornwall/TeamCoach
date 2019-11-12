d3dda5d8a0164a084276b35415e7436dbd299ce8
Trenton Cornwall <trenton434@gmail.com>
2019-11-12 16:03:00 -0700

    Working on update

--- client/src/components/Admin/User/index.js
+++ client/src/components/Admin/User/index.js
@@ -3,34 +3,94 @@ import Popup from "reactjs-popup";
 import "./style.css";
 import { userInfo } from "os";
 
-function User(props) {
-  return (
-    <div className="userRow">
-      <p id={props.id} type={props.type} key={props.id} team={props.team}>
-        {props.fullName}
-      </p>
-      <button
-        className="userButton delete"
-        id={props.id}
-        onClick={props.delete}
-      >
-        Delete
-      </button>
-      <Popup
-        className="popup"
-        modal
-        trigger={<button className="userButton update modal">Update</button>}
-      >
-        <h1>{props.fullName}</h1>
-        <hr />
-        <br />
-        <p> ID : {props.id} </p>
-        <p> Email:</p>
-        <input value={props.email} placeholder={props.email}></input>
-        <p> Team: {props.teamID} </p>
-        <button>Save </button>
-      </Popup>
-    </div>
-  );
+class User extends React.Component {
+  state = {
+    email: "",
+    userType: 0,
+    teamID: ""
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+  render() {
+    return (
+      <div className="userRow">
+        <p
+          id={this.props.id}
+          type={this.props.type}
+          key={this.props.id}
+          team={this.props.team}
+        >
+          {this.props.fullName}
+        </p>
+        <button
+          className="userButton delete"
+          id={this.props.id}
+          onClick={this.props.delete}
+        >
+          Delete
+        </button>
+
+        <Popup
+          className="popup"
+          modal
+          trigger={<button className="userButton update modal">Update</button>}
+        >
+          {/* https://hackernoon.com/create-react-modal-using-reactjs-popup-m24m231v1 */}
+          <h1>{this.props.fullName}</h1>
+          <hr />
+          <div className="popupcontent">
+            <div className="poplabel">
+              <p>
+                ID: <span className="popData">{this.props.id} </span>
+              </p>
+            </div>
+            <div className="poplabel">
+              <p>email:</p>
+              <input
+                className="popInput"
+                name="email"
+                value={this.props.email}
+                placeholder={this.props.email}
+              ></input>
+            </div>
+            <p> User Type: </p>
+            <select
+              className="popDropDown"
+              name="userType"
+              onChange={this.handleInputChange}
+            >
+              <option value="0">Base (0)</option>
+              <option value="1">Supervisor (1)</option>
+              <option value="2">Manager (2)</option>
+              <option value="3">Admin (3)</option>
+            </select>
+          </div>
+
+          <div className="poplabel">
+            <p>Team:</p>
+            <select
+              className="popDropDown"
+              name="userType"
+              onChange={this.handleInputChange}
+            >
+              <option value="0">Example Team</option>
+              <option value="1">Example Team 2</option>
+              <option value="2">Example Team 3</option>
+              <option value="3">Example Team 4</option>
+            </select>
+          </div>
+
+          <button>Save </button>
+        </Popup>
+      </div>
+    );
+  }
 }
 export default User;
