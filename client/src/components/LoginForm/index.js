2aff6756079ac72254c11580a9f0c1109d0e9bf7
KSK <kurtknosala@gmail.com>
2019-11-12 19:58:46 -0700

    front page styling complete

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -4,14 +4,12 @@ import "./index.css";
 function Login() {
 	return (
     <div>
-        <h1><img src="./images/logo.svg" alt="Logo" className="logo"/> Team<span className="companyName">Coach</span></h1>
+        <h1 className="loginTitle"><img src="./images/logo.svg" alt="Logo" className="logo"/> Team<span className="companyName">Coach</span></h1>
         <form>
-            User Name:<br />
-            <input type="text"></input>
-            <br />
-            Password:<br />
-            <input type="password"></input>
-            <br /><br />
+            <p className="loginFieldTitle" >User Name:</p>
+            <input type="text" className="loginField" ></input>
+            <p className="loginFieldTitle" >Password:</p>
+            <input type="password" className="loginField" ></input>
             <input type="submit" value="Login" className="loginSubmit"></input>
         </form>
     </div>
