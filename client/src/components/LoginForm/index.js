a52759d3924c3d23d7e7fbb4bc4070cb787b5306
KSK <kurtknosala@gmail.com>
2019-11-12 18:45:47 -0700

    styling work on login continuing

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -2,9 +2,20 @@ import React from "react";
 import "./index.css";
 
 function Login() {
-	return <div>
-        <h1>This is a test</h1>
+	return (
+    <div>
+        <h1><img src="./images/logo.svg" alt="Logo" className="logo"/> Team<span className="companyName">Coach</span></h1>
+        <form>
+            User Name:<br />
+            <input type="text"></input>
+            <br />
+            Password:<br />
+            <input type="password"></input>
+            <br /><br />
+            <input type="submit" value="Login" className="loginSubmit"></input>
+        </form>
     </div>
+    )
 }
 
 export default Login;
