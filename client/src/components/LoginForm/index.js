2401cecb9b95c983731788a2eacf063a7a138759
KSK <kurtknosala@gmail.com>
2019-11-14 19:43:19 -0700

    auth work

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import "./index.css";
 
 class Login extends Component {
     state = {
-        userName: "",
+        email: "",
         password: ""
     }
     
-    componentDidMount() {
-
-    }
 
     handleInputChange = event => {
         const{name,value} = event.target
@@ -20,8 +18,14 @@ class Login extends Component {
 
     handleFormSubmit = event => {
         event.preventDefault();
-        console.log(this.state.userName)
-        console.log(this.state.password)
+        axios
+            .get('login/auth', {
+                email: this.state.email,
+                password: this.state.password
+            })
+            .then(response =>{
+                console.log(response)
+            })
     }
 
     render() {
@@ -30,7 +34,7 @@ class Login extends Component {
                 <h1 className="loginTitle"><img src="./images/logo.svg" alt="Logo" className="logo"/> Team<span className="companyName">Coach</span></h1>
                 <form>
                     <p className="loginFieldTitle" >User Name:</p>
-                    <input type="text" className="loginField" name="userName" value={this.state.userName} onChange={this.handleInputChange}></input>
+                    <input type="text" className="loginField" name="email" value={this.state.email} onChange={this.handleInputChange}></input>
                     <p className="loginFieldTitle" >Password:</p>
                     <input type="password" className="loginField" name="password" value={this.state.password} onChange={this.handleInputChange}></input>
                     <input type="submit" value="Login" className="loginSubmit" onClick={this.handleFormSubmit}></input>
