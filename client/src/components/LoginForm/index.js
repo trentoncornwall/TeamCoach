db2217939625658a31f4b7a362400948e8cc415e
KSK <kurtknosala@gmail.com>
2019-11-14 18:20:18 -0700

    input change handled

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./index.css";
 
-function Login() {
-	return (
-    <div>
-        <h1 className="loginTitle"><img src="./images/logo.svg" alt="Logo" className="logo"/> Team<span className="companyName">Coach</span></h1>
-        <form>
-            <p className="loginFieldTitle" >User Name:</p>
-            <input type="text" className="loginField" ></input>
-            <p className="loginFieldTitle" >Password:</p>
-            <input type="password" className="loginField" ></input>
-            <input type="submit" value="Login" className="loginSubmit"></input>
-        </form>
-    </div>
-    )
+class Login extends Component {
+    state = {
+        userName: "",
+        password: ""
+    }
+    
+    handleInputChange = event => {
+        const{name,value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 className="loginTitle"><img src="./images/logo.svg" alt="Logo" className="logo"/> Team<span className="companyName">Coach</span></h1>
+                <form>
+                    <p className="loginFieldTitle" >User Name:</p>
+                    <input type="text" className="loginField" name="userName" value={this.state.userName} onChange={this.handleInputChange}></input>
+                    <p className="loginFieldTitle" >Password:</p>
+                    <input type="password" className="loginField" name="passWord" value={this.state.passWord} onChange={this.handleInputChange}></input>
+                    <input type="submit" value="Login" className="loginSubmit"></input>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Login;
