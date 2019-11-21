e2fbb26f123ebfc5247a3d1f0374542f721d0d29
kknosala <51036712+kknosala@users.noreply.github.com>
2019-11-20 21:25:39 -0700

    file setup for auth

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,25 @@ import Main from "./pages/Main";
 import Plan from "./pages/Plan";
 import NoMatch from "./pages/NoMatch";
 import Teams from "./pages/Teams";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/main" component={Main} />
-          <Route exact path="/plan/:id" component={Plan} />
-          <Route exact path="/admin" component={Admin} />
-          <Route exact path="/teams" component={Teams} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/main" component={Main} />
+            <Route exact path="/plan/:id" component={Plan} />
+            <Route exact path="/admin" component={Admin} />
+            <Route exact path="/teams" component={Teams} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
+import errorReducer from "./errorReducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default combineReducers({
+  auth: authReducer,
+  errors: errorReducer
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
