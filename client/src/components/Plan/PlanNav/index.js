ec413e1a19b329076cc3ce9abfe355fb84731fe9
amazingefren <efrentheroker@hotmail.com>
2019-11-21 05:03:07 -0700

    fixed css renderings, blur effect added

--- client/src/components/Plan/PlanNav/index.js
+++ client/src/components/Plan/PlanNav/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 function PlanNav(props) {
   return (
     <div className="PlanNav">
-      <span className="name">{props.navState.name}</span>
-      <span className="date">Start: {props.navState.startDate}</span>
-      <span className="date">End: {props.navState.endDate}</span>
+      <span className="PlanNav-name">{props.navState.name}</span>
+      {/* <span className="date">Start: {props.navState.startDate}</span> */}
+      {/* <span className="date">End: {props.navState.endDate}</span> */}
     </div>
   );
 }
